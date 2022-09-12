3ffb43906905152abef5ccdc1277fb594f3ca14a
AlexeyVarenko <alexey_varenko@mail.ru>
2022-09-12 12:35:44 +0300

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,35 @@
+/*Add Rounded Corners with border-radius
+Your cat photo currently has sharp corners. We can round out those corners with a CSS property called border-radius.
+
+You can specify a border-radius with pixels. Give your cat photo a border-radius of 10px.
+
+Note: This challenge allows for multiple possible solutions. For example, you may add border-radius to either the .thick-green-border class or the .smaller-image class.
+
+*/
+
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+<style>
+  .red-text {
+    color: red;
+  }
+
+  h2 {
+    font-family: Lobster, monospace;
+  }
+
+  p {
+    font-size: 16px;
+    font-family: monospace;
+  }
+
+  .thick-green-border {
+    border-color: green;
+    border-width: 10px;
+    border-style: solid;
+    border-radius: 10px;
+  }
+
+  .smaller-image {
+    width: 100px;
+  }
+</style>
